857d0d27658621237a50f44fb4c819cf589f107b
ze25800000 <ze258100000@sina.com>
2017-12-07 12:34:36 +0800

    3-3 后台验证码功能开发

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Login extends Controller {
+    public function index() {
+        return $this->fetch();
+    }
+
+    public function check() {
+        $data = input('post.');
+        if (!captcha_check($data['code'])) {
+            $this->error('验证码不正确');
+        }
+    }
+}
