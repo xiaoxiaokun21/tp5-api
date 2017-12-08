37901d4d2c52438177de9507c7577a7d3431c04d
ze25800000 <ze258100000@sina.com>
2017-12-08 18:18:09 +0800

    4-6 新闻内容添加功能实现

--- application/admin/controller/News.php
+++ application/admin/controller/News.php
@@ -4,7 +4,31 @@ namespace app\admin\controller;
 
 
 class News extends Base {
-    public function add() {
+    public function index() {
         return $this->fetch();
+    }
+
+    public function add() {
+        if (request()->isPost()) {
+            $data = input('post.');
+            // 数据校验
+            // 入库
+            try {
+                $id = model('News')->add($data);
+            } catch (\Exception $e) {
+                return $this->result('', 0, '新增失败');
+            }
+            if ($id) {
+                return $this->result([
+                    'jump_url' => url('news/index')
+                ], 1, 'OK');
+            } else {
+                return $this->result('', 0, '新增失败');
+            }
+        } else {
+            return $this->fetch('', [
+                'cats' => config('cat.lists')
+            ]);
+        }
     }
 }
--- application/common/model/AdminUser.php
+++ application/common/model/AdminUser.php
@@ -3,14 +3,6 @@
 namespace app\common\model;
 
 
-use think\Model;
-
-class AdminUser extends Model {
-    public function add($data) {
-        if (!is_array($data)) {
-            exception('传递数据不合法');
-        }
-        $this->allowField(true)->save($data);
-        return $this->getLastInsID();
-    }
+class AdminUser extends Base {
+    protected $autoWriteTimestamp = true;
 }
